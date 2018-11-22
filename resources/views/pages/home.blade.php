cbc92f4faad7d327d130c5bedcfa3782301a56f8
Nambs91 <nambsrnts@gmail.com>
2018-11-22 12:02:00 +0300

    Configure app name

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.default', ['title' => 'Home'])
 
 @section('container')
-    <h1 class="text-center">Laracarte</h1>
+    <h1 class="text-center">{{ config('app.name') }}</h1>
 @stop
