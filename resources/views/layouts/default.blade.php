4104a192bdf5ef55493e97c86b90226c1a639be6
Nambs91 <nambsrnts@gmail.com>
2018-11-22 11:38:13 +0300

    Add page_title helper

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,8 +4,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Laracarte</title>
 
+        <title>{{ page_title($title ?? '') }}</title>
+        
         <!-- google fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
         <!-- Font Awesome -->
